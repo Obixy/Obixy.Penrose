4ad0aab352d6fe96f7ee88cd104a232698debb09
“guiherme” <guilherme@agapesistemas.com.br>
2024-10-05 16:50:49 -0300

    Adicionar foco automático ao iframe na visualização do ambiente

--- src/WebUI/src/views/environment/view.tsx
+++ src/WebUI/src/views/environment/view.tsx
@@ -1,14 +1,27 @@
+import { useRef, useEffect } from "react";
 import { Sidebar, OptionsBar } from "../components";
 
 export function View() {
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+  useEffect(() => {
+    const iframe = iframeRef.current;
+
+    if (iframe) {
+      iframe.onload = function setIframeFocus() {
+        iframe.focus();
+      };
+    }
+  }, []);
+
   return (
     <div className="relative">
       <Sidebar />
 
       <iframe
+        ref={iframeRef}
         src="https://nsac-obixy-penrose-f8bygpb0bmavcxez.brazilsouth-01.azurewebsites.net/"
         className="w-full h-screen object-cover"
-        autoFocus
       ></iframe>
 
       <OptionsBar onUnitChange={(unit: number) => console.log(unit)} />
